c90b82b237a7ca65e50f31dc9b52ba0a6236b206
Feronixer <oshie.coder96@gmail.com>
2025-03-24 10:18:11 +0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,28 @@
+<div class="well card-footer">
+  <p><center>TOM YANG UNIVERSITY Founded on 2025</center>
+      <span class="float-center"></span>
+  </p>
+</div>
+
+
+
+
+  </body>
+
+
+
+
+  <!-- Jquery script -->
+  <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap.min.js"></script>
+  <script src="assets/jquery.dataTables.min.js"></script>
+  <script src="assets/dataTables.bootstrap4.min.js"></script>
+  <script>
+      $(document).ready(function () {
+          $("#flash-msg").delay(7000).fadeOut("slow");
+      });
+      $(document).ready(function() {
+          $('#example').DataTable();
+      } );
+  </script>
+</html>
